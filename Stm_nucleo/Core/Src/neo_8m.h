47244bd7ac836dbb77d6116a220440826542233c
Karthik Kunchakuri <40692309+karthikkunchakuri@users.noreply.github.com>
2025-12-17 11:56:25 +0100

    Add header file for NEO-M8N GPS module

--- Stm_nucleo/Core/Src/neo_8m.h
+++ Stm_nucleo/Core/Src/neo_8m.h
@@ -1,1 +1,12 @@
+#ifndef NEO_M8N_H
+#define NEO_M8N_H
+
+#include "stm32f4xx_hal.h"
+
+#define GPS_BUFFER_SIZE 256
+
+void GPS_Init(UART_HandleTypeDef *huart);
+void GPS_ReadLine(char *line, uint16_t max_len);
+
+#endif
 
