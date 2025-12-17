d752860d02a47b2f1b51b3d5408f0edb90c6e9d8
Karthik Kunchakuri <40692309+karthikkunchakuri@users.noreply.github.com>
2025-12-17 11:56:07 +0100

    Add GPS initialization and reading functions

--- Stm_nucleo/Core/Src/neo_8m.c
+++ Stm_nucleo/Core/Src/neo_8m.c
@@ -1,1 +1,28 @@
+#include "neo_m8n.h"
+#include <string.h>
+
+static UART_HandleTypeDef *gps_uart;
+
+void GPS_Init(UART_HandleTypeDef *huart)
+{
+    gps_uart = huart;
+}
+
+void GPS_ReadLine(char *line, uint16_t max_len)
+{
+    uint8_t ch;
+    uint16_t idx = 0;
+
+    while (idx < max_len - 1)
+    {
+        HAL_UART_Receive(gps_uart, &ch, 1, HAL_MAX_DELAY);
+
+        if (ch == '\n')
+        {
+            break;
+        }
+        line[idx++] = ch;
+    }
+    line[idx] = '\0';
+}
 
