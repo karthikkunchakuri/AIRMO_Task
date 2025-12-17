e60d5c99a01c90f59f9c4c5b20d69bf9e33bf0c5
Karthik Kunchakuri <40692309+karthikkunchakuri@users.noreply.github.com>
2025-12-17 11:59:03 +0100

    Add header file for ICM20948 accelerometer

--- Stm_nucleo/Core/Src/icm20948.h
+++ Stm_nucleo/Core/Src/icm20948.h
@@ -1,1 +1,16 @@
+#ifndef ICM20948_H
+#define ICM20948_H
+
+#include "stm32f4xx_hal.h"
+
+typedef struct {
+    int16_t ax;
+    int16_t ay;
+    int16_t az;
+} ICM20948_Accel_t;
+
+void ICM20948_Init(I2C_HandleTypeDef *hi2c);
+void ICM20948_ReadAccel(ICM20948_Accel_t *accel);
+
+#endif
 
